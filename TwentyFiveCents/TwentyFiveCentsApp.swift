aa60147769250f8116353b8f258b6427bfdfd4c1
‮lehcieR treboR <rreichel3@gmail.com>
2022-10-26 23:23:50 -0400

    Initial Commit

---
+++ TwentyFiveCents/TwentyFiveCentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyFiveCentsApp.swift
+//  TwentyFiveCents
+//
+//  Created by Robert Reichel on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TwentyFiveCentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
